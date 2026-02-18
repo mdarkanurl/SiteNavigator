b92c2972aef6bb9332cda79c2013a7a63f61438a
Mohammad Arkan <mdarkanurl@gmail.com>
2026-02-18 16:12:12 +0600

    Added logic for check url protocol

--- src/CLI/parse-input.ts
+++ src/CLI/parse-input.ts
@@ -22,12 +22,20 @@ export function parseInput(line: string): ParseResult {
         return { success: false, error: "navigate requires a URL" };
       }
 
-      let url: URL | null = null;
+      let url: URL;
       try {
         url = new URL(args[0]);
       } catch (error) {
         return { success: false, error: "Invalid URL" };
       }
+
+      if (url.protocol !== "http:"
+        && url.protocol !== "https:") {
+          return {
+            success: false,
+            error: "Invalid URL protocol. Only http and https are allowed." 
+          };
+        }
 
       return {
         success: true,
